34b2f27e5e354f5848b3d87ffa24ed6ea0ace23f
MuchContact <422549477@qq.com>
2022-09-22 23:18:06 +0800

    binding mq

--- bindings/go/mq/main.go
+++ bindings/go/mq/main.go
@@ -12,16 +12,16 @@ import (
 	"time"
 )
 type Order struct {
-	orderId string `json:"orderId"`
+	OrderId string `json:"orderId"`
 }
 //code
 func getCheckout(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var order Order
 	err := json.NewDecoder(r.Body).Decode(&order)
-	log.Println("Received Message: ", order.orderId)
+	log.Println("Received Message: ", order.OrderId)
 	time.Sleep(time.Second * 10)
-	log.Println("Received Message Done: ", order.orderId)
+	log.Println("Received Message Done: ", order.OrderId)
 	if err != nil {
 		log.Printf("error parsing checkout input binding payload: %s", err)
 		w.WriteHeader(http.StatusOK)
